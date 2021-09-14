9d685edafa91e355c1f91718f1722eb91dc36f21
DESKTOP-DE23UL5\joann <joannasl95@hotmail.com>
2021-09-14 11:14:39 +0200

    psudocode from ta added

--- main.go
+++ main.go
@@ -1,5 +1,43 @@
 package main
 
 func main() {
+	//we need to be able to ask with certain int values
+	// 0 = how many times have you been used/eaten
+	// -1 = are you being used/are you eating
 
+}
+
+//channelse. start med channels, og så bag efter kig på at optælle
+
+func main(){
+	ch1,ch2 :_ make(chan string), make (chan string)
+	go fork (ch1, ch2)
+	go phil(ch2, ch1)
+	for{
+		//eat forever
+	}
+}
+
+
+func phil(chIN, chOut){
+	counter:= 0
+	for{
+	chOUT <- i wanna eat	
+	<-chIN
+	counter++
+	chOUT <- im done
+
+	}
+}
+
+func fork(chIN, chOUT){
+	for{
+		<-chIN
+		//besked om at samlpe op
+
+		chOUT <- "spis, yes"
+
+		//hvornår er jeg lagt igen?
+		<-chIN	
+	}
 }
--- philosophers.go
+++ philosophers.go
@@ -18,3 +18,22 @@ type Philosopher struct{
 	var eating bool
 }
 
+func phil(chIN, chOut){
+	counter:= 0
+	for{
+	chOUT <- i wanna eat	
+	<-chIN
+	counter++
+	chOUT <- im done
+
+	}
+}
+
+//channel fra phil til gaffel. tre tjeks i alt. er du ledig? ja. nu er jeg færidg. og så til begge sider
+//ch1:=make(chan string)
+	//<- chIN - jeg vil gerne modtage. når den får en besked kan den skrive til chOUT "tag mig", som phil ser
+	//når phil så er færdig får den endnu en besked på chIN "nu er jeg done"
+//med unbuffered står den stille og venter på der kommer en besked. først når der kommer en går den
+//videre. og slef skal der være en goroutine der skal læse inputtet
+
+
